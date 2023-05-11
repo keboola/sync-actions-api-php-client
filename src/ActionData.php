577bdfcc3f899612220a3ce0bfc89fbbc5680a08
Ondra <ondra.jodas@gmail.com>
2023-05-11 08:38:50 +0200

    fix action data

--- src/ActionData.php
+++ src/ActionData.php
@@ -29,7 +29,7 @@ class ActionData
     public function getArray(): array
     {
         return [
-            'component' => $this->componentId,
+            'componentIdk' => $this->componentId,
             'action' => $this->action,
             'configData' => $this->configData,
             'tag' => $this->tag,
